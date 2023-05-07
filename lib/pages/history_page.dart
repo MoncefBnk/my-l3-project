4a9b26f5cf5a0041bec7c1c282953c84c3826a92
Moncyyy <moncef-benkhalfallah@outlook.com>
2023-05-07 17:17:30 +0100

    History page finished

--- lib/pages/history_page.dart
+++ lib/pages/history_page.dart
@@ -1,3 +1,6 @@
+// ignore_for_file: prefer_const_constructors
+
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
@@ -15,6 +18,29 @@ class HistorPage extends StatefulWidget {
 
 class _HistorPageState extends State<HistorPage> {
   @override
+  void initState() {
+    super.initState();
+    getHistory();
+  }
+
+  final FirebaseFirestore firestore = FirebaseFirestore.instance;
+
+  Future<QuerySnapshot> getHistory() async {
+    int x = DateTime.now()
+        .subtract(Duration(days: 30))
+        .toUtc()
+        .millisecondsSinceEpoch;
+    QuerySnapshot qShot = await firestore
+        .collection('sensor')
+        .doc(widget.historyID)
+        .collection('history')
+        .orderBy('time', descending: true)
+        .get();
+
+    return qShot;
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
@@ -23,7 +49,29 @@ class _HistorPageState extends State<HistorPage> {
           style: GoogleFonts.cairo(fontWeight: FontWeight.bold),
         ),
       ),
-      body: ListView(),
+      body: FutureBuilder<QuerySnapshot>(
+        future: getHistory(),
+        builder: (context, snapshot) {
+          if (snapshot.hasData) {
+            return ListView.builder(
+              itemCount: snapshot.data!.docs.length,
+              itemBuilder: (context, index) {
+                DocumentSnapshot doc = snapshot.data!.docs[index];
+                DateTime time = doc['time'].toDate();
+                return ListTile(
+                  title: Text('Temperature: ${doc['temperature'].toString()}'),
+                  subtitle: Text('Humidity: ${doc['humidity'].toString()}'),
+                  trailing: Text('Time: $time'),
+                );
+              },
+            );
+          } else if (snapshot.hasError) {
+            return Text('Error: ${snapshot.error}');
+          } else {
+            return CircularProgressIndicator();
+          }
+        },
+      ),
     );
   }
 }
