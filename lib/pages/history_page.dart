ce989cbb432d1998659903c26277b06bac6609dd
Moncyyy <moncef-benkhalfallah@outlook.com>
2023-05-26 16:01:27 +0100

    historydone

--- lib/pages/history_page.dart
+++ lib/pages/history_page.dart
@@ -1,8 +1,9 @@
-// ignore_for_file: prefer_const_constructors
+// ignore_for_file: prefer_const_constructors, unnecessary_string_interpolations
 
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:intl/intl.dart';
 
 class HistorPage extends StatefulWidget {
   const HistorPage({
@@ -53,17 +54,43 @@ class _HistorPageState extends State<HistorPage> {
         future: getHistory(),
         builder: (context, snapshot) {
           if (snapshot.hasData) {
-            return ListView.builder(
-              itemCount: snapshot.data!.docs.length,
-              itemBuilder: (context, index) {
-                DocumentSnapshot doc = snapshot.data!.docs[index];
-                DateTime time = doc['time'].toDate();
-                return ListTile(
-                  title: Text('Temperature: ${doc['temperature'].toString()}'),
-                  subtitle: Text('Humidity: ${doc['humidity'].toString()}'),
-                  trailing: Text('Time: $time'),
-                );
-              },
+            return Padding(
+              padding: EdgeInsets.only(top: 20),
+              child: ListView.builder(
+                itemCount: snapshot.data!.docs.length,
+                itemBuilder: (context, index) {
+                  DocumentSnapshot doc = snapshot.data!.docs[index];
+                  DateTime time = doc['time'].toDate();
+                  var formatter = DateFormat('MMM d, y h:mm a');
+                  var formattedTime = formatter.format(time);
+                  return SizedBox(
+                    height: 80,
+                    child: ListTile(
+                      title: Text('$formattedTime'),
+                      subtitle: Padding(
+                        padding: EdgeInsets.symmetric(vertical: 9),
+                        child: Row(
+                          children: [
+                            Icon(
+                              Icons.thermostat,
+                              size: 30,
+                              color: Colors.red,
+                            ),
+                            Text('${doc['temperature'].toString()} °C'),
+                            SizedBox(width: 20),
+                            Icon(
+                              Icons.water_drop,
+                              size: 30,
+                              color: Colors.blue,
+                            ),
+                            Text('${doc['humidity'].toString()} %'),
+                          ],
+                        ),
+                      ),
+                    ),
+                  );
+                },
+              ),
             );
           } else if (snapshot.hasError) {
             return Text('Error: ${snapshot.error}');
